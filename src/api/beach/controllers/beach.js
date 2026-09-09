17c53afb23b4c3f4b34d13167bcb36a2266196d5
KNR1997 <kethakaranasinghe@gmail.com>
2026-09-09 08:16:09 +0530

    chore: remove unnecessary

--- src/api/beach/controllers/beach.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * beach controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::beach.beach');
--- src/api/beach/routes/beach.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * beach router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::beach.beach');
